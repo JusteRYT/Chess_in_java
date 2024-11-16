4d729beb0c85c32bddd2c0e8d9cce4328aecaeb8
Sergey <93572822+JusteRYT@users.noreply.github.com>
2024-11-17 01:25:03 +0300

    Добавлена логика проверки ходов для ферзя и короля

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,47 +7,35 @@ public class Main {
         System.out.println("Шахматная доска инициализирована!");
         System.out.println(board);
 
-        // Пробуем сделать ход Пешкой
-        System.out.println("Ход ладьей (a2 -> a4):");
-        if (board.movePiece(6, 0, 4, 0)) { // a1 -> a4
-            System.out.println("Ход успешен!");
+        System.out.println("Ход пешкой (e2 -> e4)");
+        if(board.movePiece(6,4,4,4)){
+            System.out.println("Ход выполнен!");
         } else {
-            System.out.println("Ход не выполнен.");
-        }
-        System.out.println(board);
-        // Пробуем сделать ход ладьей
-        System.out.println("Ход ладьей (a1 -> a3):");
-        if (board.movePiece(7, 0, 5, 0)) { // a1 -> a4
-            System.out.println("Ход успешен!");
-        } else {
-            System.out.println("Ход не выполнен.");
+            System.out.println("Ход не выполнен!");
         }
         System.out.println(board);
 
-
-        // Пробуем сделать ход Пешкой
-        System.out.println("Ход пеш (b2 -> b4):");
-        if (board.movePiece(6, 1, 4, 1)) { // a1 -> a4
-            System.out.println("Ход успешен!");
+        System.out.println("Ход пешкой (с2 -> c4");
+        if(board.movePiece(6,2,4,2)){
+            System.out.println("Ход выполнен!");
         } else {
-            System.out.println("Ход не выполнен.");
-        }
-        System.out.println(board);
-        // Пробуем сделать ход слоном
-        System.out.println("Ход слоном (c1 -> f4):");
-        if (board.movePiece(7, 2, 6, 1)) { // c1 -> f4
-            System.out.println("Ход успешен!");
-        } else {
-            System.out.println("Ход не выполнен.");
+            System.out.println("Ход не выполнен!");
         }
         System.out.println(board);
 
-        // Пробуем сделать ход конем
-        System.out.println("Ход конем (b1 -> c3):");
-        if (board.movePiece(7, 1, 5, 2)) { // b1 -> c3
-            System.out.println("Ход успешен!");
+        System.out.println("Ход королем (e1 -> e2");
+        if(board.movePiece(7,4,6,4)){
+            System.out.println("Ход выполнен!");
         } else {
-            System.out.println("Ход не выполнен.");
+            System.out.println("Ход не выполнен!");
+        }
+        System.out.println(board);
+
+        System.out.println("Ход ферзем (d7 -> a4");
+        if(board.movePiece(7, 3, 4,0)){
+            System.out.println("Ход выполнен!");
+        } else {
+            System.out.println("Ход не выполнен!");
         }
         System.out.println(board);
     }
